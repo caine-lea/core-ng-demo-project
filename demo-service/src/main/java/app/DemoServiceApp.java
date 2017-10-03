2a48d63021e45af8fa6631ef7d858db73429ed88
neo <neowu.us@gmail.com>
2017-10-03 14:02:14 +0800

    revert http test (tested on gcloud debian 9, okHTTP does better in multi thread, but still not match apache http client performance with POST)

--- demo-service/src/main/java/app/DemoServiceApp.java
+++ demo-service/src/main/java/app/DemoServiceApp.java
@@ -10,8 +10,8 @@ public class DemoServiceApp extends App {
     @Override
     protected void initialize() {
         load(new SystemModule("sys.properties"));
-        http().httpPort(8080);
-        http().httpsPort(8443);
+//        http().httpPort(8080);
+//        http().httpsPort(8443);
         load(new ProductModule());
 //        load(new JobModule());
     }
